f00e2a0257559c4a34237c7c75532c44288d45ed
Dominik <nikicresl@gmail.com>
2015-08-17 17:34:03 +0200

    test

--- mathematic/forms.py
+++ mathematic/forms.py
@@ -11,19 +11,3 @@ class BrigadeForm(forms.Form):
 	class Meta:
 		model = Day
 		fields = ('brigade', 'number_of_day', 'hours_per_day', 'pub_date')
-
-	def __init__(self, *args, **kwargs):
-     	super(BrigadeForm, self).__init__(*args, **kwargs)
-     	self.helper = FormHelper()
-     	self.helper.layout = Layout(
-        Fieldset(
-            'Main',
-            'brigade',
-            'number_of_day',
-            'hours_per_day',
-            'pub_date'
-        ),
-        ButtonHolder(
-            Submit('submit', 'Submit', css_class='button white')
-        )
-     )
