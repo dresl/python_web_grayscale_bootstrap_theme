ae5c05f2e039a3571df1542db388270ae12f856d
Dominik <nikicresl@gmail.com>
2015-08-17 17:12:04 +0200

    add crispy forms

--- mathematic/forms.py
+++ mathematic/forms.py
@@ -1,8 +1,29 @@
 from django import forms
 from mathematic.models import Brigade, Day
 
+from crispy_forms.bootstrap import Tab, TabHolder
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import (HTML, ButtonHolder, Field, Fieldset, Layout,
+                                 Submit)
+
 class BrigadeForm(forms.ModelForm):
 
 	class Meta:
 		model = Day
 		fields = ('brigade', 'number_of_day', 'hours_per_day', 'pub_date')
+
+	def __init__(self, *args, **kwargs):
+        super(BrigadeForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                'Main',
+                'brigade',
+                'number_of_day',
+                'hours_per_day',
+                'pub_date'
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit', css_class='button white')
+            )
+        )
