c861f4a809cbd00c6bc68c26a16216a96ef8dbe7
dresl <nikicresl@gmail.com>
2015-11-08 12:21:24 +0100

    add new server run

--- server_run.sh
+++ server_run.sh
@@ -1,8 +1,11 @@
 #!bin/bash
-if python manage.py runserver 192.168.0.103:80 ; then
+
+ip=$(ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+
+if python manage.py runserver $ip:80 ; then
     echo "Command succeeded"
 else
     echo "Command failed, preparing comand fuser -k 80/tcp"
     fuser -k 80/tcp
-    python manage.py runserver 192.168.0.103:80
+    python manage.py runserver $ip:80
 fi
