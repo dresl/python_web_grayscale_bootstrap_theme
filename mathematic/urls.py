063a42322c5a8c872e660b741e3a0ca2e078c778
dresl <nikicresl@gmail.com>
2015-11-10 18:35:02 +0100

    active improve

--- mathematic/urls.py
+++ mathematic/urls.py
@@ -4,7 +4,7 @@ from mathematic import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),  
-    url(r'^create/$', views.create, name='createb'),
+    url(r'^brigade/create/$', views.create, name='createb'),
     url(r'^brigade/$', views.BrigadeIndexView.as_view(), name='brigade_index'),
     url(r'^brigade/(?P<pk>\d+)/$', views.DetailView, name='detailb'),
     url(r'^calc/$', views.CalcIndexView.as_view(), name='calc_index'),
