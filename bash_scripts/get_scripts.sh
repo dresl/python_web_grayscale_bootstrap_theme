a37ec4a1e4e50c720c12c73b20058db0c3a07397
dresl <nikicresl@gmail.com>
2016-04-15 09:09:05 +0200

    add get_script

---
+++ bash_scripts/get_scripts.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+wget https://github.com/dresl/python_web_grayscale_bootstrap_theme/blob/master/bash_scripts/refresh_skin.sh
+wget https://github.com/dresl/python_web_grayscale_bootstrap_theme/blob/master/bash_scripts/refresh_template.sh
+wget https://github.com/dresl/python_web_grayscale_bootstrap_theme/blob/master/bash_scripts/server_run.sh
