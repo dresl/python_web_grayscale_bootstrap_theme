c12e33e7d8fea0e223fe39a9ea582a6978f6e0fb
dresl <nikicresl@gmail.com>
2016-04-15 09:24:24 +0200

    improve get_scripts

--- bash_scripts/get_scripts.sh
+++ bash_scripts/get_scripts.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-wget https://github.com/dresl/python_web_grayscale_bootstrap_theme/blob/master/bash_scripts/refresh_skin.sh
-wget https://github.com/dresl/python_web_grayscale_bootstrap_theme/blob/master/bash_scripts/refresh_template.sh
-wget https://github.com/dresl/python_web_grayscale_bootstrap_theme/blob/master/bash_scripts/server_run.sh
+wget https://raw.githubusercontent.com/dresl/python_web_grayscale_bootstrap_theme/master/bash_scripts/refresh_skin.sh
+wget https://raw.githubusercontent.com/dresl/python_web_grayscale_bootstrap_theme/master/bash_scripts/refresh_template.sh
+wget https://raw.githubusercontent.com/dresl/python_web_grayscale_bootstrap_theme/master/bash_scripts/server_run.sh
