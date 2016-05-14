67ec1a4b8b2ad84f71b22dfbefe2bd21e2790cd8
Dominik Resl <nikicresl@gmail.com>
2016-05-14 09:35:18 +0200

    create modul_install

---
+++ bash_scripts/module_install.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+. bin/activate
+pip install -e git+https://github.com/leonardo-modules/leonardo-theme-grayscale.git@#egg=leonardo_theme_grayscale
+pip install -e git+https://github.com/dresl/leonardo-scrolltop.git@#egg=leonardo_scrolltop
+pip install -e git+https://github.com/leonardo-modules/leonardo-iconlink.git@#egg=leonardo_iconlink
+pip install -e git+https://github.com/leonardo-modules/leonardo-module-blog@#egg=leonardo_module_blog
+pip install -e git+https://github.com/leonardo-modules/leonardo-module-forms@#egg=leonardo_module_forms
+pip install -e git+https://github.com/leonardo-modules/leonardo-geo@#egg=leonardo_geo
