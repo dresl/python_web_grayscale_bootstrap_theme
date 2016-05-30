da2053be8a39845ab16c9aa619dd30a2c349752c
Dominik Resl <nikicresl@gmail.com>
2016-05-30 21:34:19 +0200

    Update module_install.sh

--- bash_scripts/module_install.sh
+++ bash_scripts/module_install.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 . bin/activate
 pip install -e git+https://github.com/leonardo-modules/leonardo-theme-grayscale.git@#egg=leonardo_theme_grayscale
-pip install -e git+https://github.com/dresl/leonardo-scrolltop.git@#egg=leonardo_scrolltop
+pip install -e git+https://github.com/leonardo-modules/leonardo-scrolltop#egg=leonardo_scrolltop
 pip install -e git+https://github.com/leonardo-modules/leonardo-iconlink.git@#egg=leonardo_iconlink
 pip install -e git+https://github.com/leonardo-modules/leonardo-module-blog@#egg=leonardo_module_blog
 pip install -e git+https://github.com/leonardo-modules/leonardo-module-forms@#egg=leonardo_module_forms
